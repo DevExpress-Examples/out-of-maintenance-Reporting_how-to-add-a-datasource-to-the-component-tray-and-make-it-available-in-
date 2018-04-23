9857d7b817998c2cbd34fb6302d5fdcd698b094e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraReports.Design;
+using DevExpress.XtraReports.UserDesigner;
+using EUD_AddDataSource.nwindDataSetTableAdapters;
+// ...
+
+namespace EUD_AddDataSource {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        nwindDataSet ds = new nwindDataSet();
+        ProductsTableAdapter adapter = new ProductsTableAdapter();
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Create an End-User Designer form.
+            XRDesignFormEx form = new XRDesignFormEx();
+
+            // Create a report instance.
+            XtraReport1 report = new XtraReport1();
+
+            // Add data components to the Designer Host.
+            form.DesignPanel.DesignerHostLoaded += 
+                new DesignerLoadedEventHandler(OnDesignerLoaded);
+
+            // Load the report into the End-User Designer.
+            form.OpenReport(report);
+
+            // Bind the report to data.
+            report.DataSource = ds;
+            report.DataAdapter = adapter;
+            report.DataMember = "Products";            
+            
+            // Show the End-User Designer form.
+            form.ShowDialog();
+        }
+
+        private void OnDesignerLoaded(object sender, DesignerLoadedEventArgs e) {
+            ReportDesigner.AddToContainer(e.DesignerHost, ds);
+            ReportDesigner.AddToContainer(e.DesignerHost, adapter);
+        }
+
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to add a datasource to the Component Tray and make it available in the Property Grid of the End-User Designer
+
+
+<p>The following example demonstrates how to manually add components to the <i>designer host</i> of the End-User Report Designer. This may be required, for instance, if a report initially isn't bound to data, and data components are provided to it later - either after calling the <strong>XRDesignFormEx.OpenReport</strong> method or after clicking a custom button in the End-User Designer. In this case, data components will not be displayed in the Component Tray, and it will be impossible to assign them to a report's DataSource and DataAdapter properties until they are added to the <i>designer host</i>.</p>
+
+
+<h3>Description</h3>
+
+<p>To solve this task, it is necessary to handle the <strong>XRDesignFormEx.DesignPanel.DesignerHostLoaded</strong> event and call the <strong>ReportDesigner.AddToContainer</strong> method in this event handler for every data component, you want to add to the designer host.</p><p><strong>Note</strong>: If you want to remove any items from the designer host, call the <strong>ReportDesigner.RemoveFromContainer</strong> method.</p>
+
+<br/>
+
+
